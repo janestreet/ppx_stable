0b37137ca1e8424bac38b35af5816676b5149acb
Arseniy Alekseyev <aalekseyev@janestreet.com>
2021-07-30 17:30:20 +0100

    v0.15~preview.124.41+380

--- src/ppx_stable.ml
+++ src/ppx_stable.ml
@@ -365,16 +365,47 @@ let convert_variant
   acc
 ;;
 
-let conversions_of_td
-      ~ppx_name
-      ~target_type
-      ~rec_flag
-      ?(add = [])
-      ?(remove = [])
-      ?(modify = [])
-      ?(set = [])
-      td
-  =
+module Changes_by_type = struct
+  type 'a t =
+    { add : 'a
+    ; modify : 'a
+    ; set : 'a
+    ; remove : 'a
+    }
+
+  type kind =
+    | Add
+    | Modify
+    | Set
+    | Remove
+
+  let set t kind value =
+    match kind with
+    | Add -> { t with add = value }
+    | Modify -> { t with modify = value }
+    | Set -> { t with set = value }
+    | Remove -> { t with remove = value }
+  ;;
+
+  let get t kind =
+    match kind with
+    | Add -> t.add
+    | Modify -> t.modify
+    | Set -> t.set
+    | Remove -> t.remove
+  ;;
+
+  let create x = { add = x; modify = x; set = x; remove = x }
+
+  let map t ~f =
+    { add = f t.add; modify = f t.modify; set = f t.set; remove = f t.remove }
+  ;;
+
+  let to_list t = [ t.add; t.modify; t.set; t.remove ]
+end
+
+let conversions_of_td ~ppx_name ~target_type ~rec_flag changes td =
+  let ({ add; modify; set; remove } : _ Changes_by_type.t) = changes in
   let loc = td.ptype_loc in
   let add = Set.of_list (module String) add in
   let modify = Set.of_list (module String) modify in
@@ -511,7 +542,7 @@ let conversions_of_td
   extra_struct @ conversions
 ;;
 
-let fields_or_constructors =
+let fields_or_constructors () =
   let open Ast_pattern in
   let rec_fields_pat = elist (pexp_ident (lident __)) in
   let constrs_pat = elist (pexp_construct (lident __) none) in
@@ -532,14 +563,80 @@ let type_pattern =
   alt ident type_
 ;;
 
+let stable_changes =
+  let raise_invalid_change_argument ~loc =
+    Location.raise_errorf
+      ~loc
+      "Invalid change argument. Expected add, modify, set, or remove."
+  in
+  Attribute.declare
+    "stable.changes"
+    Type_declaration
+    Ast_pattern.(pstr (pstr_eval (pexp_apply (estring (string "")) __) nil ^:: nil))
+    (fun args : _ Changes_by_type.t ->
+       let init = Changes_by_type.create None in
+       List.fold args ~init ~f:(fun acc (label, expression) ->
+         let loc = expression.pexp_loc in
+         let name =
+           match label with
+           | Labelled name -> name
+           | Nolabel | Optional _ -> raise_invalid_change_argument ~loc
+         in
+         let kind : Changes_by_type.kind =
+           match name with
+           | "add" -> Add
+           | "modify" -> Modify
+           | "set" -> Set
+           | "remove" -> Remove
+           | _ -> raise_invalid_change_argument ~loc
+         in
+         let value =
+           Ast_pattern.parse (fields_or_constructors ()) loc expression Fn.id
+         in
+         match Changes_by_type.get acc kind with
+         | None -> Changes_by_type.set acc kind (Some value)
+         | Some _ -> Location.raise_errorf ~loc "%s argument was passed twice" name)
+       |> Changes_by_type.map ~f:(Option.value ~default:[]))
+;;
+
+let make_stable_changes_attribute
+      ~loc
+      ?(add = [])
+      ?(modify = [])
+      ?(set = [])
+      ?(remove = [])
+      ()
+  =
+  let open (val Ast_builder.make loc) in
+  let mkident x =
+    if Char.is_lowercase x.[0]
+    then pexp_ident (Located.lident x)
+    else pexp_construct (Located.lident x) None
+  in
+  let ident_list names = elist (List.map ~f:mkident names) in
+  let change_expression =
+    pexp_apply
+      [%expr ""]
+      [ Labelled "add", ident_list add
+      ; Labelled "set", ident_list set
+      ; Labelled "modify", ident_list modify
+      ; Labelled "remove", ident_list remove
+      ]
+  in
+  attribute
+    ~name:(Located.mk (Attribute.name stable_changes))
+    ~payload:(PStr [ pstr_eval change_expression [] ])
+;;
+
 let args =
   Deriving.Args.(
+    let changes = pack2 (pexp_loc __ (fields_or_constructors ())) in
     empty
     +> arg "version" type_pattern
-    +> arg "add" fields_or_constructors
-    +> arg "modify" fields_or_constructors
-    +> arg "set" fields_or_constructors
-    +> arg "remove" fields_or_constructors)
+    +> arg "add" changes
+    +> arg "modify" changes
+    +> arg "set" changes
+    +> arg "remove" changes)
 ;;
 
 (* That's actually useless, it's just here so ppxlib's driver doesn't complain *)
@@ -557,7 +654,24 @@ let () = Driver.register_transformation "stable" ~extensions:[ rewrite_type_ext 
 let gen ppx_name ~loc ~path:_ (rec_flag, tds) target_type add modify set remove =
   match tds with
   | [ td ] ->
-    conversions_of_td ~rec_flag ~ppx_name ~target_type ?add ?remove ?modify ?set td
+    let changes_from_args : _ Changes_by_type.t = { add; modify; set; remove } in
+    let changes =
+      match Attribute.get stable_changes td with
+      | Some changes_from_attribute ->
+        (match Changes_by_type.to_list changes_from_args |> List.find_map ~f:Fn.id with
+         | None -> ()
+         | Some (loc, _) ->
+           Location.raise_errorf
+             ~loc
+             "The changes (add, modify, set, or remove) passed to\n\
+              [@@@@deriving %s] are unnecessary. They are already\n\
+              specified by the [@@@@stable.changes] attribute."
+             ppx_name);
+        changes_from_attribute
+      | None ->
+        Changes_by_type.map changes_from_args ~f:(Option.value_map ~f:snd ~default:[])
+    in
+    conversions_of_td ~rec_flag ~ppx_name ~target_type changes td
   | _ ->
     Location.raise_errorf
       ~loc
